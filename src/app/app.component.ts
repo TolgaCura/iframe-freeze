a76885633722c31e3f7e484e40d64e7b6fec5a75
Tolga Cura <mrtolgacura@gmail.com>
2022-05-04 15:02:03 +0100

    added some styling changes to demo iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+declare global {
+  interface Window {
+    iFrameResize:any;
+  }
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iframe-freeze';
+
+  ngOnInit() {
+    window.iFrameResize({ log: true, checkOrigin: false, autoResize: true, enablePublicMethods: true }, '#bb-journey');
+    // pass url params to the iframe
+    const params: string = window.location.search.replace("?", "&");
+    const iframe = document.getElementById("bb-journey") as HTMLIFrameElement;
+
+    console.log('iframe', iframe)
+
+    iframe.src += params;
+  }
 }
